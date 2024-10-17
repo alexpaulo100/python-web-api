a19ca43be2b6961db9783ccf09ce2e03e1da69c4
alexpaulo100 <alex.paulo100@gmail.com>
2024-10-17 19:33:25 -0300

    add files

--- exemplos/day2/flask/blog/posts.py
+++ exemplos/day2/flask/blog/posts.py
@@ -1,12 +1,13 @@
 from datetime import datetime
 
+import pymongo
 from blog.database import mongo
 from unidecode import unidecode
 
 
 def get_all_posts(published: bool = True):
     posts = mongo.db.posts.find({"published": published})
-    return posts.sort("date")
+    return posts.sort("date", pymongo.DESCENDING)
 
 
 def get_post_by_slug(slug: str) -> dict:
---
+++ exemplos/day2/flask/blog/views.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, Flask, abort, redirect, render_template, request, url_for
+
+from blog.posts import get_all_posts, get_post_by_slug, new_post
+
+bp = Blueprint("post", __name__, template_folder="templates")
+
+
+@bp.route("/")
+def index():
+    posts = get_all_posts()
+    return render_template("index.html.j2", posts=posts)
+
+
+@bp.route("/<slug>")
+def detail(slug):
+    post = get_post_by_slug(slug)
+    if not post:
+        return abort(404, "Post not found")
+    return render_template("post.html.j2", post=post)
+
+
+@bp.route("/new", methods=["GET", "POST"])
+def new():
+    if request.method == "POST":
+        title = request.form.get("title")
+        content = request.form.get("content")
+        slug = new_post(title, content)
+        return redirect(url_for("post.detail", slug=slug))
+    return render_template("form.html.j2")
+
+
+def configure(app: Flask):
+    app.register_blueprint(bp)
